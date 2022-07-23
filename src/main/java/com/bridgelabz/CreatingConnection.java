e63d31e07e84d0aa2d256fde0d6b94bfd63381e9
amit <amitpanika97@gmail.com>
2022-07-23 20:38:37 +0530

    first commit

---
+++ src/main/java/com/bridgelabz/CreatingConnection.java
@@ -0,0 +1,35 @@
+package com.bridgelabz;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class CreatingConnection {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+       
+        try {
+            
+            String url = "jdbc:mysql://localhost:3306/salary";
+            String username = "root";
+            String password = "Amitpanika97@#";
+            Connection connection = null;
+
+            //  loading the Driver...........................
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // Creating Connection...........................
+
+            connection = DriverManager.getConnection(url, username, password);
+            if (connection.isClosed()) {
+                System.out.println(" Connection closed");
+            } else {
+                System.out.println(" Connection created .....");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
