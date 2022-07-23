55f97150cc604f27c2cf70803c904690e3c94437
amit <amitpanika97@gmail.com>
2022-07-23 20:41:15 +0530

    Connecting javaDatabaseConnectivity

--- src/main/java/InsertJdbc.java
+++ src/main/java/InsertJdbc.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class InsertJdbc {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class InsertJdbc {
+    public static void main(String[] args) {
+
+        try
+        {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // Create a connection..........
+
+            String url = "jdbc:mysql://localhost:3306/javademo";
+            String name = "root";
+            String password = "Amitpanika97@#";
+
+            Connection connection = DriverManager.getConnection(url,name,password);
+
+            // create a query..............
+
+            // create a statement..........
+
+            Statement statement = connection.createStatement();
+            statement.executeUpdate("create table table5( id int(20) not null)");
+            statement.executeUpdate("create table table3( id int(20) not null)");
+            statement.executeUpdate("create table table4( id int(20) not null)");
+            System.out.println("table created in databases......");
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+    }
 }
--- src/main/java/jdbc2.java
+++ src/main/java/jdbc2.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class jdbc2 {
+import java.sql.*;
+
+public class jdbc2 {
+    public static void main(String[] args) throws SQLException {
+        try {
+        String url = "jdbc:mysql://localhost:3306/payroll";
+        String username = "root";
+        String password = "Amitpanika97@#";
+        Connection connection=null;
+
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(url,username,password);
+            if ( connection.isClosed()){
+                System.out.println("closed");
+            }else {
+                System.out.println("created ....................");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
