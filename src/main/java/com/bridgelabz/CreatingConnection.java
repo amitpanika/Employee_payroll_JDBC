bf8f72d52e58a13073185b31871563c503678582
amit <amitpanika97@gmail.com>
2022-07-24 13:42:22 +0530

    updating the salary in databases

--- src/main/java/com/bridgelabz/CreatingConnection.java
+++ src/main/java/com/bridgelabz/CreatingConnection.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 
 
 public class CreatingConnection {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+    public static void main(String[] args)  {
        
         try {
             
--- src/main/java/com/bridgelabz/UpdateTheSalary.java
+++ src/main/java/com/bridgelabz/UpdateTheSalary.java
@@ -4,21 +4,30 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
 
-public class AddData {
+public class UpdateTheSalary {
     public static void main(String[] args) {
 
-        try{
+        try {
+
+            //  load the Driver..............
+
             Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // create connection.............
+
             String url = "jdbc:mysql://localhost:3306/salary";
             String name = "root";
             String password = "Amitpanika97@#";
+
             Connection connection = DriverManager.getConnection(url, name, password);
             Statement statement = connection.createStatement();
-            statement.executeUpdate("create table employee(id int(20) not null, name varchar(20) not null)");
-            statement.executeUpdate("insert into employee(id, name) values (1,'bill'), (2,'Mark'), (3,'charlie')");
-            System.out.println("Table created in databases.....");
-            connection.close();
-        }catch ( Exception e){
+            statement.executeUpdate("alter table employee add Salary double not null");
+
+            // Updating salary................
+
+            statement.executeUpdate("update employee set salary = 300000.00 where name = 'bill'");
+            System.out.println("table created in database.......");
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
